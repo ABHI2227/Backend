81eb257168bdde306a83d88dffc4a13d98d38106
Abhishek <malveabhishek2000@gmail.com>
2025-01-13 23:19:27 +0530

    mvc

---
+++ MVC/Server.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import path from 'path';
+import ejs from 'ejs';
+import mongoose from 'mongoose';
+
+const app = express();
+
+// Connect to MongoDB
+mongoose.connect('mongodb+srv://abhishekmalve27022:iELngosmU6CyAprw@cluster0.o7vrb.mongodb.net/', {
+    dbName: "NodeJs Mastery Course"
+}
+).then(() => {
+    console.log('Connected to MongoDB');
+}).catch((error) => {
+    console.error('Error connecting to MongoDB:', error);
+}); 
+
+const port = 3000;
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
