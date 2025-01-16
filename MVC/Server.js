92092ad76fe58dcfad0740b7904ee5e65b619769
Abhishek <malveabhishek2000@gmail.com>
2025-01-16 07:39:45 +0530

    post

--- MVC/Server.js
+++ MVC/Server.js
@@ -2,8 +2,11 @@ import express from 'express';
 import path from 'path';
 import ejs from 'ejs';
 import mongoose from 'mongoose';
+import {User}  from './Models/User.js';
+
 
 const app = express();
+app.use(express.urlencoded({ extended: true }));
 
 // Connect to MongoDB
 mongoose.connect('mongodb+srv://abhishekmalve27022:iELngosmU6CyAprw@cluster0.o7vrb.mongodb.net/', {
@@ -14,6 +17,26 @@ mongoose.connect('mongodb+srv://abhishekmalve27022:iELngosmU6CyAprw@cluster0.o7v
 }).catch((error) => {
     console.error('Error connecting to MongoDB:', error);
 }); 
+
+
+app.get( '/', (req, res) => {
+    res.render('index.ejs');
+})
+
+app.post('/form-submit', (req, res) => {
+    console.log(req.body);
+    res.json({
+       message: 'your form has been submited'
+
+    })
+    try {
+        const user = await User.create(req.body);
+        console.log(user);
+    } catch (error) {
+        console.error(error);
+    }   
+})
+
 
 const port = 3000;
 
